21f466a9e663fafc83d00f0cede79a8d3eb6bf5b
Vivek Kumar Chaudhary <chaudharv101@gmail.com>
2018-03-18 00:18:08 +0530

    docs: build prototype

---
+++ fcfs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct node
+{
+	int cylinder;
+	node *next;
+} *front, *rear;
+
+int queueIt(int cylinderData){
+	retun 0;
+}
+
+int dequeueIt(){
+	return 0;
+}
+
+int main(){
+	front = NULL; rear = NULL;
+	return 0;
+}
