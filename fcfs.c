f29ac442a57342b657db8d846f5576c085717203
Vivek Kumar Chaudhary <chaudharv101@gmail.com>
2018-03-18 00:41:52 +0530

    docs: In main(), initialise variables and invoke queueIt(int a[],int s)

--- fcfs.c
+++ fcfs.c
@@ -4,9 +4,9 @@ struct node
 {
 	int cylinder;
 	node *next;
-} *front, *rear;
+} *front, *rear, *ptr;
 
-int queueIt(int cylinderData){
+int queueIt(int cylinderArray[], int size){
 	retun 0;
 }
 
@@ -16,5 +16,9 @@ int dequeueIt(){
 
 int main(){
 	front = NULL; rear = NULL;
+	int numberOfProcess = 9, head = 143, totalDistance = 0;
+	int cylinderArray[] = {86, 1470, 913, 1774, 948, 1509, 1022, 1750, 130};
+
+	queueIt(cylinderArray, numberOfProcess);
 	return 0;
 }
