a8f897e6b5b5694a1168e43d2f490d2a7b79a042
Vivek Kumar Chaudhary <chaudharv101@gmail.com>
2018-03-19 10:11:58 +0530

    Modify: modify heap from linked list to array

--- priority.c
+++ priority.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
 struct Node {
+	int arivalTime;
+	int bursttime;
 	int remainingServiceTime;
 	int priority;
 	struct Node *left, *right;
-} *root;
+} *root, *ptr;
+
+int minheapify(struct Node *inserted) {
+	return 0;
+}
+
+int maxheapify(struct Node *inserted) {
+	return 0;
+}
+
+int insertIntoHeap(int process) {
+	return 0;
+}
+
+int deleteFromHeap() {
+	return 0;
+}
+
+int findMaxPriority() {
+	return 0;
+}
+*/
+int main() {
+	int numberOfProcess;
+	printf("Enter number of processes : \n");
+	scanf("%d", &numberOfProcess);
+	return 0;
+}
